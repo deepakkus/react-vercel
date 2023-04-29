c8b4435d025e1940d0c39a79111f4f6a457713a6
sumanafrontend <sumana@kusmail.com>
2023-04-29 19:15:01 +0530

    code updated

--- server/app.js
+++ server/app.js
@@ -54,19 +54,7 @@ app.use('/', indexRouter);
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-// Your code
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
-// Your code
+
 
 
 app.listen(8090, () => {
